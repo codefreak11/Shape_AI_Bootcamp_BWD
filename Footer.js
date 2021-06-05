958481996ce08be0f9fe3fb209924d556d9d4f69
codefreak11 <64731004+codefreak11@users.noreply.github.com>
2021-06-05 13:54:17 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Footer(){
+   
+  var year = new Date().getFullYear()
+   return(
+     <footer>
+        copyright@{year}
+     </footer>
+   
+   )
+
+}
+
+export default Footer
